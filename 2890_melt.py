04b182b8bd22ca6ddc43bcd43d5e509fbe0f0b4a
Aditya Kumar Jha <163677863+AdityaCypherX@users.noreply.github.com>
2026-02-10 21:16:41 +0530

    Create 2890_melt.py

---
+++ 2890_melt.py
@@ -0,0 +1,15 @@
+"""
+Problem ID: 2890
+Title: Reshape Data: Melt
+Platform: LeetCode
+Difficulty: Easy
+Topic: Pandas | Reshaping | Melt
+"""
+import pandas as pd
+
+def meltTable(report: pd.DataFrame) -> pd.DataFrame:
+    return report.melt(id_vars=["product"],
+    value_vars=["quarter_1","quarter_2","quarter_3","quarter_4"],
+    var_name="quarter",
+    value_name="sales"
+    )    
